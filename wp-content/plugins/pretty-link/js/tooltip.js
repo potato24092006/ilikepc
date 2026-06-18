6536853b70af6687370ec21b8b375ac03a929cd8
Potato <le1330348@gmail.com>
2026-06-18 19:42:45 +0700

    Done

--- wp-content/plugins/pretty-link/js/tooltip.js
+++
@@ -1,9 +0,0 @@
-jQuery(function ($) {
-    tippy('.prli-tooltip', {
-        content: (reference) => reference.getAttribute('data-title'),
-        allowHTML: true,
-        trigger: 'mouseenter click',
-        interactive: true,
-
-      });
-});
